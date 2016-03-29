55f5cba9e8513f6a824235cb9f8ba2a15827a275
Yoonjung <yyj4094yyj@naver.com>
2016-03-29 20:46:28 +0900

    int -> number

--- WebContent/META-INF/SQL/member.sql
+++ WebContent/META-INF/SQL/member.sql
@@ -4,7 +4,7 @@ create table member_table(
 m_id varchar2(20),
 m_pw varchar2(20) not null,
 m_name varchar2(10) not null,
-m_phone int(15) not null,
+m_phone number(15) not null,
 m_email varchar2(20) not null,
 m_address varchar2(50) not null,
 primary key(m_id)
--- WebContent/META-INF/SQL/product.sql
+++ WebContent/META-INF/SQL/product.sql
@@ -4,8 +4,8 @@ create table product_table(
 p_num number,
 p_name varchar2(20) not null,
 p_contents varchar2(1000) not null,
-p_price int(10) not null,
-p_quantity int(10) not null,
+p_price number(10) not null,
+p_quantity number(10) not null,
 p_color varchar2(10) not null,
 primary key(p_num)
 );
